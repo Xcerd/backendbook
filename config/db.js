81534f367047da83c84d7cceb63e48c18ec23302
Xcerd <travisdg646@gmail.com>
2025-02-14 21:53:01 -0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 import mysql from 'mysql2/promise';
 
 const connection = await mysql.createConnection({
-  host: process.env.DB_HOST,      // Use environment variables
+  host: trolley.proxy.rlwy.net,      // Use environment variables
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME

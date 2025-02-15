67dd1dbab200d9de4d5eb365bdf75db4ba073d90
Xcerd <travisdg646@gmail.com>
2025-02-14 22:12:46 -0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,9 +2,9 @@ import mysql from 'mysql2/promise';
 
 const connection = await mysql.createConnection({
   host: "trolley.proxy.rlwy.net",  // ✅ FIXED - Now it's a valid string
-  user: root,
-  password: OmksFVATaWEHnewLXiXmQPMfHJmexZye,
-  database: railway
+  user: "root",
+  password: "OmksFVATaWEHnewLXiXmQPMfHJmexZye",
+  database: "railway"
 });
 
 export default db;
